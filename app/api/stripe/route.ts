6e35c6e32706cb351a28ef84781c6f1c37273d3f
enigma-devhub <emmanuelonoja291@gmail.com>
2023-12-06 17:35:33 +0100

    dsds

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,7 +1,4 @@
-// pages/api/stripe/route.ts
-
 import { auth, currentUser } from "@clerk/nextjs";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
@@ -10,35 +7,61 @@ import { absoluteUrl } from "@/lib/utils";
 
 const settingsUrl = absoluteUrl("/settings");
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export async function GET() {
   try {
     const { userId } = auth();
     const user = await currentUser();
 
     if (!userId || !user) {
-      return new Response("Unauthorized", { status: 401 });
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
     const userSubscription = await prismadb.userSubscription.findUnique({
       where: {
-        userId,
-      },
-    });
+        userId
+      }
+    })
 
     if (userSubscription && userSubscription.StripeCustomerId) {
       const stripeSession = await stripe.billingPortal.sessions.create({
         customer: userSubscription.StripeCustomerId,
         return_url: settingsUrl,
-      });
+      })
 
-      return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+      return new NextResponse(JSON.stringify({ url: stripeSession.url }))
     }
-  } catch (error) {
-    console.log("STRIPE_ERROR: ", error);
 
-    return new NextResponse("Internal error", { status: 500 });
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: settingsUrl,
+      cancel_url: settingsUrl,
+      payment_method_types: ["card"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      customer_email: user.emailAddresses[0].emailAddress,
+      line_items: [
+        {
+          price_data: {
+            currency: "USD",
+            product_data: {
+              name: "Genius Pro",
+              description: "Unlimited AI Generations"
+            },
+            unit_amount: 2000,
+            recurring: {
+              interval: "month"
+            }
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId,
+      },
+    })
+
+    return new NextResponse(JSON.stringify({ url: stripeSession.url }))
+  } catch (error) {
+    console.log("[STRIPE_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
-}
+};
