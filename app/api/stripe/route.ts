229a63760b02b3e6a6719997d8e3b0574b6c3d72
enigma-devhub <emmanuelonoja291@gmail.com>
2023-12-06 15:21:43 +0100

    hg

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,44 +1,44 @@
-// pages/api/stripe/route.ts
+// // pages/api/stripe/route.ts
 
-import { auth, currentUser } from "@clerk/nextjs";
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+// import { auth, currentUser } from "@clerk/nextjs";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { NextResponse } from "next/server";
 
-import prismadb from "@/lib/prismadb";
-import { stripe } from "@/lib/stripe";
-import { absoluteUrl } from "@/lib/utils";
+// import prismadb from "@/lib/prismadb";
+// import { stripe } from "@/lib/stripe";
+// import { absoluteUrl } from "@/lib/utils";
 
-const settingsUrl = absoluteUrl("/settings");
+// const settingsUrl = absoluteUrl("/settings");
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    const { userId } = auth();
-    const user = await currentUser();
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   try {
+//     const { userId } = auth();
+//     const user = await currentUser();
 
-    if (!userId || !user) {
-      return new Response("Unauthorized", { status: 401 });
-    }
+//     if (!userId || !user) {
+//       return new Response("Unauthorized", { status: 401 });
+//     }
 
-    const userSubscription = await prismadb.userSubscription.findUnique({
-      where: {
-        userId,
-      },
-    });
+//     const userSubscription = await prismadb.userSubscription.findUnique({
+//       where: {
+//         userId,
+//       },
+//     });
 
-    if (userSubscription && userSubscription.StripeCustomerId) {
-      const stripeSession = await stripe.billingPortal.sessions.create({
-        customer: userSubscription.StripeCustomerId,
-        return_url: settingsUrl,
-      });
+//     if (userSubscription && userSubscription.StripeCustomerId) {
+//       const stripeSession = await stripe.billingPortal.sessions.create({
+//         customer: userSubscription.StripeCustomerId,
+//         return_url: settingsUrl,
+//       });
 
-      return new NextResponse(JSON.stringify({ url: stripeSession.url }));
-    }
-  } catch (error) {
-    console.log("STRIPE_ERROR: ", error);
+//       return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+//     }
+//   } catch (error) {
+//     console.log("STRIPE_ERROR: ", error);
 
-    return new NextResponse("Internal error", { status: 500 });
-  }
-}
+//     return new NextResponse("Internal error", { status: 500 });
+//   }
+// }
