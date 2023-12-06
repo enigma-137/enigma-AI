3bb8931285c456a7b9d2d5a8eb28c0b7759c4d3f
enigma-devhub <emmanuelonoja291@gmail.com>
2023-12-06 08:01:04 +0100

    hgh

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,44 +1,44 @@
-import { auth, currentUser } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+// import { auth, currentUser } from "@clerk/nextjs";
+// import { NextResponse } from "next/server";
 
-import prismadb from "@/lib/prismadb";
+// import prismadb from "@/lib/prismadb";
 
-import { stripe } from "@/lib/stripe";
-import { absoluteUrl } from "@/lib/utils";
+// import { stripe } from "@/lib/stripe";
+// import { absoluteUrl } from "@/lib/utils";
 
 
 
-const settingsUrl = absoluteUrl("/settings")
+// const settingsUrl = absoluteUrl("/settings")
 
-export async function Get(){
-    try{
-        const {userId} = auth()
-        const user = await currentUser()
+// export async function Get(){
+//     try{
+//         const {userId} = auth()
+//         const user = await currentUser()
 
-        if(!userId || !user){
-            return new Response("Unauthorized", {status: 401})
-        } 
+//         if(!userId || !user){
+//             return new Response("Unauthorized", {status: 401})
+//         } 
 
-        const userSubscription = await prismadb.userSubscription.findUnique({
-            where: {
-                userId
-            }
-        })
-        if(userSubscription && userSubscription.StripeCustomerId){
+//         const userSubscription = await prismadb.userSubscription.findUnique({
+//             where: {
+//                 userId
+//             }
+//         })
+//         if(userSubscription && userSubscription.StripeCustomerId){
 
-            const stripeSession = await stripe.billingPortal.sessions.create({
-                customer: userSubscription.StripeCustomerId,
-                return_url: settingsUrl
+//             const stripeSession = await stripe.billingPortal.sessions.create({
+//                 customer: userSubscription.StripeCustomerId,
+//                 return_url: settingsUrl
 
-            })
+//             })
 
-            return new NextResponse(JSON.stringify({url: stripeSession.url}))
+//             return new NextResponse(JSON.stringify({url: stripeSession.url}))
 
-        }
+//         }
 
-    }catch(error){
-        console.log("STRIPE_ERROR: ", error);
+//     }catch(error){
+//         console.log("STRIPE_ERROR: ", error);
 
-        return new NextResponse("Internal error", {status: 500})
-    }
-}
+//         return new NextResponse("Internal error", {status: 500})
+//     }
+// }
